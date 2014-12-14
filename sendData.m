a9df6fcf5c6502272ab2e710ec7f2adda6c8a96e
phiaseitz <sophia.seitz@students.olin.edu>
2014-12-14 09:58:02 -0500

    Packetizing data to send

--- sendData.m
+++ sendData.m
@@ -1,6 +1,6 @@
-function[sendable] = sendData(rate, frequency, data)
-%NOTE: not actually tested yet -- this might require some debugging
-%Create daq session
+function sendable = sendData(datafile,rate)
+%Start up the Daq
+
 s = daq.createSession('digilent')
 s.DurationInSeconds =120;
 %This is W1. 
@@ -9,28 +9,54 @@ ch = addAnalogOutputChannel(s,'AD1', 1, 'Voltage')
 %Get the rate
 s.Rate = rate;
 
-%How many copies of the same number we need
-numcopies = rate/frequency;
+%define header
+header = [1 0 1 0 1 0 1 0];
+%length without header
+packetlen = 128;
+%For now, just read in all at once. 
+data = csvread(datafile);
+%Scale appropriately
+data = 10*data-5;
+%Number of packets
+numpackets = ceil(length(data)/packetlen);
 
-%Make that many copies of data% 
-%tiled = repmat(data,numcopies);
+%% Transmit once
 
-%concatenate column after column %creates a giant vector that crashes
-%matlab. don't do this
-outdata = reshape(tiled', 1,numel(data))
+% for i = 1:numpackets
+%     startind = packetlen*(i -1) + 1;
+%     if (i == numpackets)
+%         datatotransmit = horzcat(header,data(startind:end))';
+%     else
+%         endind = startind + (packetlen - 1);
+%         datatotransmit = horzcat(header,data(startind:endind))';
+%     end
+%     
+%     queueOutputData(s,datatotransmit);
+%     s.startForeground;
+%   
+% end
 
-%Eventually we'll concatenate the outdata with a header here
-output = outdata;
-sendable = str2double(cellstr(output'))'
 
+%% Transmit Continuously
 
-% onesarray = 5*ones(rate,1);
-% zerosarray = zeros(rate,1);
-% 
-% output = cat(1,onesarray,zerosarray);
+% iteration = 0;
+% while true
+%     iteration = iteration +1;
+%     i = mod(iteration, numpackets) + 1;
+%     startind = packetlen*(i -1) + 1;
+%     if (i == numpackets)
+%         datatotransmit = horzcat(header,data(startind:end))';
+%     else
+%         endind = startind + (packetlen - 1);
+%         datatotransmit = horzcat(header,data(startind:endind))';
+%     end
+%     
+%     queueOutputData(s,datatotransmit);
+%     s.startForeground;
+%   
+% end
 
+%% Return on completion
+sendable = 1;
 
-%Transmit through the Analog Discovery
-queueOutputData(s,output);
-s.startForeground;
 end
